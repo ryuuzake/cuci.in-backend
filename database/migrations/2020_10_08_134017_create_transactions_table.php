bf45ebdbfca8fa4ce768609f5dc2f7a3116fe79b
tudeverdhangga <tudeverdhangga@gmail.com>
2020-10-08 21:52:05 +0800

    add transaction migration

---
+++ database/migrations/2020_10_08_134017_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->integer('price')->nullable();
+            $table->float('amount')->nullable();
+            $table->string('status')->nullable();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('outlet_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
