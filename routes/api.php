56c6af8ec8328d8d5fd77339b37856b782aede5a
Sekigahara <androaprila12@gmail.com>
2020-10-15 10:45:38 +0700

    API Owner(Create, Show all and Show by id)

--- routes/api.php
+++ routes/api.php
@@ -43,5 +43,9 @@ Route::group(['prefix' => 'auth'], function () {
         Route::put('/outlet/{outlet}', 'OutletController@update');
         Route::delete('/outlet/{outlet}', 'OutletController@destroy');
         
+        // Owner
+        Route::get('/owner', 'OwnerController@index');
+        Route::post('/owner', 'OwnerController@store');
+        Route::get('/owner/{owner}', 'OwnerController@show');
     });
 });
